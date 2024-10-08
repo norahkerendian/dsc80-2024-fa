b065b8236f13b221dd9c33864ea0a7c30afe8611
norahkerendian <norahkerendian@gmail.com>
2024-10-08 09:31:59 -0700

    commiting

--- labs/lab01/lab.py
+++ labs/lab01/lab.py
@@ -30,7 +30,21 @@ def consecutive_ints(ints):
 
 
 def median_vs_mean(nums):
-    ...
+    nums.sort() # in-place
+    length = len(nums)
+    half_way = length // 2
+    # sort the list first then determine if the list is even or odd then find the middle number 
+    if length % 2 == 0: # even
+        list_median = (nums[half_way - 1] + nums[half_way])/2
+    else:
+        list_median = nums[half_way]
+
+    list_mean = sum(nums) / length
+
+    # print(f'mean = {list_mean}')
+    # print(f'median = {list_median}')
+    return list_median <= list_mean
+
 
 
 # ---------------------------------------------------------------------
@@ -39,7 +53,16 @@ def median_vs_mean(nums):
 
 
 def n_prefixes(s, n):
-    ...
+    # s[n-1] + s[n-2] + s[n-3] ...
+    # there has to be a way to do this recursively 
+    if n == 0:
+        # so the base case has to be n decreasing to 0
+        return ""
+    else:
+        # s is the new prefix everytime n decreases 
+        s = s[:n]
+        return s + n_prefixes(s, n-1)
+        
 
 
 # ---------------------------------------------------------------------
@@ -48,7 +71,16 @@ def n_prefixes(s, n):
 
 
 def exploded_numbers(ints, n):
-    ...
+    largest_val = max(ints) + n
+    output = []
+    for i in ints:
+        first = i - n
+        end = i + n
+        string_num = ' '.join(str(j).zfill(len(str(largest_val))) for j in range(first, end + 1))
+        output.append(string_num)
+        # str.zfill(max(len(str(largest_val))))
+    return output
+
 
 
 # ---------------------------------------------------------------------
@@ -57,7 +89,15 @@ def exploded_numbers(ints, n):
 
 
 def last_chars(fh):
-    ...
+    s = fh.read()
+    s = s.split('\n')
+    output = ''
+    for line in s:
+        if line != '':
+            last = line[-1]
+            output += last
+    return output
+        
 
 
 # ---------------------------------------------------------------------
@@ -66,10 +106,20 @@ def last_chars(fh):
 
 
 def add_root(A):
-    ...
+    # (1, 5, 7) -> (1 + sqrt(0), 5 + sqrt(1), 7 + sqrt(2))
+    # i need to make an array of the sqrts and then just add the two arrays together
+    length = len(A)
+    sqrt_array = np.sqrt(np.array(range(0, length)))
+    return A + sqrt_array
 
 def where_square(A):
-    ...
+    # [2,9,16,15] -> [False, True, True, False]
+    # sqrt(int) == int but idk if this can be done w/o a loop 
+    squared_A = np.sqrt(A)
+    squared_A_convert = squared_A.astype(int)
+    return squared_A == squared_A_convert
+
+
 
 
 # ---------------------------------------------------------------------
@@ -78,7 +128,24 @@ def where_square(A):
 
 
 def filter_cutoff_loop(matrix, cutoff):
-    ...
+
+    groups = [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+    means = [sum(i)/len(i) for i in groups]
+    index_list = []
+    for i, num in enumerate(means):
+        if num > cutoff:
+            index_list.append(i)
+ 
+    new_matrix = []      
+    for row in matrix:
+        saved_rows = []
+        for i in index_list:
+            saved_rows.append(row[i])
+        new_matrix.append(saved_rows)
+    
+    return np.array(new_matrix)
+
+
 
 
 # ---------------------------------------------------------------------
@@ -87,7 +154,9 @@ def filter_cutoff_loop(matrix, cutoff):
 
 
 def filter_cutoff_np(matrix, cutoff):
-    ...
+    means = np.mean(matrix, axis = 0)
+    return matrix[:, means > cutoff]
+
 
 
 # ---------------------------------------------------------------------
@@ -96,10 +165,25 @@ def filter_cutoff_np(matrix, cutoff):
 
 
 def growth_rates(A):
-    ...
+    diff_array = np.diff(A)
+    growth = diff_array / A[:-1]
+    return np.round(growth,2)
+# round((A[i+1]- A[i]) / A[i], 2)
+
 
 def with_leftover(A):
-    ...
+    # when leftover is greater than or equal to the stock price of the same day, return that day
+    # np.cumsum() should be used for the money that is left over. 
+    # 20 % stock price to get left over
+    # then add the left over with cumsum 
+    # but still check after everyday if you have money left over to buy stock. 
+    # if total leftover at the end (so the last element of the cumsum array) is less than the last stock day, then return -1
+    remainder = 20%A
+    leftover = np.cumsum(remainder) # this should add up every time
+    for i in range(len(A)):
+        if A[i] <= leftover[i-1]:
+            return np.int64(i + 1)
+    return np.int64(-1)
 
 
 # ---------------------------------------------------------------------
@@ -108,8 +192,38 @@ def with_leftover(A):
 
 
 def salary_stats(salary):
-    ...
+    # have a dictionary that you update and then cast it into a series
+    output_dic = {}
 
+    # 'num_players': groupby player and then count how many rows there are
+    output_dic['num_players'] = salary.groupby('Player').count().shape[0]
+
+    # 'num_teams': groupby team and then count how many rows there are
+    output_dic['num_teams'] = salary.groupby('Team').count().shape[0]
+
+    # 'total_salary': just sum up the salary column (if this fails, then maybe you have to groupby player first and then sum up salary)
+    output_dic['total_salary'] = int(salary['Salary'].sum())
+    # output_dic['total_salary_gr'] = int(salary.groupby('Player').sum()['Salary'].sum())
+
+    # 'highest_salary': find the max salary 
+    output_dic['highest_salary'] = salary.sort_values(by='Salary', ascending = False).iloc[0]['Player']
+
+    # 'avg_loss': groupby team and use mean() and then loc the Lakers and round the salary
+    output_dic['avg_los'] = float(salary.groupby('Team')['Salary'].mean().loc['Los Angeles Lakers'].round(2))
+
+    # 'fifth_lowest': sort the salary column and find the fifth row and get the name and team and then concatonate in string format. 
+    player = salary.sort_values(by = "Salary").iloc[4]['Player']
+    team = salary.sort_values(by = "Salary").iloc[4]['Team']
+    output_dic['fifth_lowest'] = player + ', ' + team
+
+    # 'duplicates': this can possibl be done with indexing and getting the 1 positon for each name and comparing and there if there is one, return true
+    just_last = salary['Player'].str.split().str[1]
+    output_dic['duplicates'] = bool(just_last.duplicated().any())
+
+    # 'total_highest': sort the salary column and go to the max value and return it's corresponding team name. then groupby team name and sum the salary.
+    team_sal = salary.sort_values(by = "Salary", ascending = False).iloc[0]['Team']
+    output_dic['total_highest'] = int(salary.groupby('Team').sum().loc[team_sal]["Salary"])
+    return pd.Series(output_dic)
 
 # ---------------------------------------------------------------------
 # QUESTION 9
@@ -117,4 +231,35 @@ def salary_stats(salary):
 
 
 def parse_malformed(fp):
-    ...
+    file = open(fp, 'r')
+    # , or " are at the end or beginning of each entry
+    df_list = []
+    cols = True
+    for i in file:
+        # reads one line at a time
+
+        # replace the "" with nothing
+        # then split the lines by the ,
+        # then check if each split list is of length 5
+        # if it is not of length 5, then loop throigh and remove empty ''
+        # then use pd.dataframe to convert it to a dataframe
+        # add together the last two columns to get the 'geo' column and format it as needed
+
+        removed_quotes = i.replace('"', '')
+        removed_enter = removed_quotes.replace('\n', '')
+        split_comma = removed_enter.split(',')
+        if len(split_comma) > 6:
+            split_comma.remove('')
+        split_comma[-2:] = [str(split_comma[-2]) + ',' + str(split_comma[-1])]
+
+        if cols == True:
+            split_comma[-1:] = split_comma[-1].split(',')
+            cols = False
+        else:
+            split_comma[2] = float(split_comma[2])
+            split_comma[3] = float(split_comma[3])
+        df_list.append(split_comma)
+
+    # df_list[0][-1:] = df_list[0][-1].split(',')
+    df = pd.DataFrame(data = df_list[1:], columns=df_list[0])
+    return df
--- projects/project01/project.py
+++ projects/project01/project.py
@@ -14,7 +14,27 @@ import plotly.express as px
 
 
 def get_assignment_names(grades):
-    ...
+    columns = grades.columns
+    syllabus = ['lab', 'project', 'midterm', 'final', 'disc', 'checkpoint']
+    output_dict = {
+        'lab': [], 
+        'project': [],
+        'midterm': [],
+        'final': [],
+        'disc': [],
+        'checkpoint': []
+        }
+
+    for col in columns:
+        if len(col.split()) == 1:
+            col_lower = col.lower()
+            for item in syllabus:
+                if item in col_lower:
+                    output_dict[item].append(col)
+    
+    output_dict['project'] = [item for item in output_dict['project'] if len(item.split('_')) == 1]
+
+    return output_dict
 
 
 # ---------------------------------------------------------------------
@@ -23,7 +43,24 @@ def get_assignment_names(grades):
 
 
 def projects_total(grades):
-    ...
+    project_names = get_assignment_names(grades)['project']
+    project_df = pd.DataFrame(index=grades.index)
+
+    for project in project_names:
+        grades[project] = grades[project].fillna(0) 
+        if f'{project}_free_response' in grades.columns:
+            grades[f'{project}_free_response'] = grades[f'{project}_free_response'].fillna(0)
+        
+        if f'{project}_free_response - Max Points' in grades.columns:
+            project_max_scores = grades[f'{project} - Max Points'] + grades[f'{project}_free_response - Max Points']
+            project_scores = grades[f'{project}'] + grades[f'{project}_free_response']
+        else:
+            project_max_scores = grades[f'{project} - Max Points']
+            project_scores = grades[f'{project}']
+
+        project_df[project] = project_scores / project_max_scores
+
+    return project_df.sum(axis = 1) / project_df.shape[1]
 
 
 # ---------------------------------------------------------------------
@@ -32,7 +69,26 @@ def projects_total(grades):
 
 
 def lateness_penalty(col):
-    ...
+    grace_time = pd.Timedelta(hours=2) # 2 hours
+    first_penalty = pd.Timedelta(weeks=1)  # 168 hours
+    second_penalty= pd.Timedelta(weeks=2) # 336 hours
+
+    col = pd.to_timedelta(col)
+
+    multiplier = pd.Series(index=col.index)
+
+    for i, time in enumerate(col):
+        # print(i, lateness)
+        if time <= grace_time:
+            multiplier[i] = 1.0  
+        elif time <= first_penalty:
+            multiplier[i] = 0.9  
+        elif time <= second_penalty:
+            multiplier[i] = 0.7 
+        else:
+            multiplier[i] = 0.4  
+
+    return multiplier
 
 
 # ---------------------------------------------------------------------
@@ -41,7 +97,12 @@ def lateness_penalty(col):
 
 
 def process_labs(grades):
-    ...
+    cols = get_assignment_names(grades)['lab'] # column names
+    output = pd.DataFrame(columns = cols, index = grades.index)
+    for lab in cols:
+        output[lab] = (grades[lab] / grades[f'{lab} - Max Points']) * lateness_penalty(grades[f'{lab} - Lateness (H:M:S)'])
+    
+    return output
 
 
 # ---------------------------------------------------------------------
@@ -50,7 +111,18 @@ def process_labs(grades):
 
 
 def lab_total(processed):
-    ...
+    scores = []
+
+    for i in range(processed.shape[0]):
+        cur_row = processed.iloc[i]
+        cur_row = cur_row.fillna(0)
+        cur_row = cur_row.drop(cur_row.idxmin())
+        total_lab_score = cur_row.sum() / len(cur_row)
+        scores.append(total_lab_score)
+
+    processed['final_score'] = scores
+    
+    return processed['final_score']
 
 
 # ---------------------------------------------------------------------
@@ -59,7 +131,35 @@ def lab_total(processed):
 
 
 def total_points(grades):
-    ...
+
+    def total_score_df(assignment_key):
+        assignments = get_assignment_names(grades)
+        list_grades = []
+
+        for item in assignments[assignment_key]:
+            grades[item] = grades[item].fillna(0)
+            item_grade = grades[item] / grades[f'{item} - Max Points']
+            list_grades.append(item_grade)
+
+        item_df = pd.DataFrame(data = list_grades, index=assignments[assignment_key]).T
+        item_df = item_df.assign(total = item_df.sum(axis = 1) / item_df.shape[1])
+        return item_df
+
+    lab_grades_final = lab_total(process_labs(grades)) * 0.20
+    project_grades_final = projects_total(grades) * 0.30
+    checkpoint_grades_final = total_score_df('checkpoint')['total'] * 0.025
+    discussion_grades_final = total_score_df('disc')['total'] * 0.025
+    midterm_grades_final = total_score_df('midterm')['total'] * 0.15
+    final_grades_final = total_score_df('final')['total'] * 0.30
+
+    course_grade = (lab_grades_final 
+                    + project_grades_final 
+                    + checkpoint_grades_final 
+                    + discussion_grades_final 
+                    + midterm_grades_final 
+                    + final_grades_final
+                    )
+    return course_grade
 
 
 # ---------------------------------------------------------------------
@@ -68,10 +168,28 @@ def total_points(grades):
 
 
 def final_grades(total):
-    ...
+
+    letter_grades = []
+    for grade in total:
+        if grade >= 0.9:
+            letter = 'A'
+        elif grade >= 0.8:
+            letter = 'B'
+        elif grade >= 0.7:
+            letter = 'C'
+        elif grade >= 0.6:
+            letter = 'D'
+        else:
+            letter = 'F'
+        letter_grades.append(letter)
+
+    return pd.Series(letter_grades)
 
 def letter_proportions(total):
-    ...
+    input_series = final_grades(total)
+    output_series = input_series.value_counts() / len(input_series)
+    output_series.sort_values(ascending=False)
+    return output_series
 
 
 # ---------------------------------------------------------------------
